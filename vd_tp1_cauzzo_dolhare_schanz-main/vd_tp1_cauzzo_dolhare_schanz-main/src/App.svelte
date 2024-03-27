9e708222171b3c07ad280bae260a337eb8b9c5e1
Joaquinschanz <165219130+Joaquinschanz@users.noreply.github.com>
2024-03-27 19:14:48 -0300

    Add files via upload

---
+++ vd_tp1_cauzzo_dolhare_schanz-main/vd_tp1_cauzzo_dolhare_schanz-main/src/App.svelte
@@ -0,0 +1,45 @@
+<!-- Script JS -->
+<script>
+    let magnitudes = [10, 25, 50, 100]
+</script>
+
+<!-- Estructura contenido HTML -->
+<div class="container">
+    {#each magnitudes as m}
+        <div class="col-wrap">
+            <div class = 'column' style='height: {m}px'></div>
+            <img class = 'mask' src="./images/tipito.svg" alt="">
+        </div>
+        
+    {/each}
+</div>
+
+
+<!-- Estilos CSS -->
+<style>
+    .container{
+        display: flex;
+        align-items: flex-end;
+        justify-content:center;
+    }
+    .column{
+        background-color: blue;
+        /*background-image: './images/agua.jpg';*/
+        width: 50px;
+        
+    }
+    .mask{
+        width: 50px;
+        height:100px; /*sale columna por abajo xq el svg tiene parte por abajo de borde de figura*/
+        position: absolute;
+        bottom: 0;
+        z-index: 1;
+    }
+    .col-wrap{
+        position: relative;
+        width: 50px;
+        margin: 5px;
+    }
+
+
+</style>
